8adb2ccd24ef4cc4b996afb20b07fe845f615435
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2025-11-17 15:33:13 +0100

    ajouter l'attribut nblikes dans Models

--- src/app/core/list-suggestion/list-suggestion.component.ts
+++ src/app/core/list-suggestion/list-suggestion.component.ts
@@ -21,7 +21,8 @@ export class ListSuggestionComponent {
       description: `Suggestion pour organiser une journée de team building pour renforcer les liens entre les membres de l'équipe.`,
       category: 'Événements',
       date: new Date('2025-01-20'),
-      status: 'acceptee'
+      status: 'acceptee',
+      nblikes : 0
     },
     {
       id: 2,
@@ -29,7 +30,8 @@ export class ListSuggestionComponent {
       description: `Proposition pour améliorer la gestion des réservations en ligne avec un système de confirmation automatique.`,
       category: 'Technologie',
       date: new Date('2025-01-15'),
-      status: 'refusee'
+      status: 'refusee',
+      nblikes : 0
     },
     {
       id: 3,
@@ -37,7 +39,8 @@ export class ListSuggestionComponent {
       description: `Mise en place d'un programme de récompenses pour motiver les employés et reconnaître leurs efforts.`,
       category: 'Ressources Humaines',
       date: new Date('2025-01-25'),
-      status: 'refusee'
+      status: 'refusee',
+      nblikes : 0
     },
     {
       id: 4,
@@ -45,7 +48,8 @@ export class ListSuggestionComponent {
       description: `Refonte complète de l'interface utilisateur pour une meilleure expérience utilisateur.`,
       category: 'Technologie',
       date: new Date('2025-01-30'),
-      status: 'en_attente'
+      status: 'en_attente',
+      nblikes : 0
     },
     {
       id: 5,
@@ -53,7 +57,8 @@ export class ListSuggestionComponent {
       description: `Organisation d'une formation sur les bonnes pratiques de sécurité informatique pour tous les employés.`,
       category: 'Formation',
       date: new Date('2025-02-05'),
-      status: 'acceptee'
+      status: 'acceptee',
+      nblikes : 0
     }
   ];
 }
